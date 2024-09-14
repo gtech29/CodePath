1c1ece97720bff8664a1579804428e8803873baf
gtech29 <juan.rodriguez.517@my.csun.edu>
2024-09-14 10:18:51 -0700

    final update

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -3,15 +3,15 @@ import Calendar from "./components/Calendar";
 
 const App = () => {
   return (
-<div className="App">
-<h1>Calendar</h1>
-<h2>
-  calendar for whatever
-</h2>
-<Calendar />
-</div>
-
-)
+    <div className="App">
+      <h1>Calendar</h1>
+      <h2>
+        This is a simple React-based calendar that displays events passed as
+        props.
+      </h2>
+      <Calendar />
+    </div>
+  );
   
 };
 
